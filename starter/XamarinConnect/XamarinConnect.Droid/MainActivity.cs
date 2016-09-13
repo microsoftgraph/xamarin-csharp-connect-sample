03599eb5779b456f7b4f11effeb9fa132f5a0013
Jim Crowley <jamescro@microsoft.com>
2016-09-13 15:58:03 -0700

    adding OnActivityResult method for Droid

--- starter/XamarinConnect/XamarinConnect.Droid/MainActivity.cs
+++ starter/XamarinConnect/XamarinConnect.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.Identity.Client;
 
 namespace XamarinConnect.Droid
 {
@@ -18,6 +19,12 @@ namespace XamarinConnect.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationAgentContinuationHelper.SetAuthenticationAgentContinuationEventArgs(requestCode, resultCode, data);
         }
     }
 }
