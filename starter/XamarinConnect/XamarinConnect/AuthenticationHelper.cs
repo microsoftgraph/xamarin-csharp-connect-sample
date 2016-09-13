a650b0595d04736702c92e52f55c6be1a7a8e36a
Jim Crowley <jamescro@microsoft.com>
2016-09-13 14:58:48 -0700

    Adding starter solution

---
+++ starter/XamarinConnect/XamarinConnect/AuthenticationHelper.cs
@@ -0,0 +1,48 @@
+﻿//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+//See LICENSE in the project root for license information.
+
+using Microsoft.Graph;
+using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace XamarinConnect
+{
+    public class AuthenticationHelper
+    {
+
+        public static string TokenForUser = null;
+        public static DateTimeOffset expiration;
+        private static GraphServiceClient graphClient = null;
+
+        // Get an access token for the given context and resourceId. An attempt is first made to 
+        // acquire the token silently. If that fails, then we try to acquire the token by prompting the user.
+        public static GraphServiceClient GetAuthenticatedClient()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Get Token for User.
+        /// </summary>
+        /// <returns>Token for user.</returns>
+        public static async Task<string> GetTokenForUserAsync()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Signs the user out of the service.
+        /// </summary>
+        public static void SignOut()
+        {
+
+
+        }
+
+    }
+}
